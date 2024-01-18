e535d689ca891a3ac7220904d50416fe2dc69b34
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-17 20:29:11 -0800

    to: Commit adding contructor of commit, adding generated date method for help generate data

--- src/gitlet/Commit.java
+++ src/gitlet/Commit.java
@@ -1,7 +1,15 @@
 package gitlet;
 
+import java.io.File;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * Commit tree class
@@ -12,10 +20,57 @@ public class Commit implements Serializable {
     String parentHash;
     // format of timestamp is Sat Nov 11 12:30:00 2017 -0800
     String timestamp;
-    String log;
+    String message;
+    /**
+     * the snapshot of blobs
+     */
     HashMap<String, String> blobs;
 
-    public Commit() {
+    private boolean initFlag = false;
 
+    public Commit(String message, String parent, HashMap<String, String> blobs, boolean initial) {
+        this.message = message;
+        this.parentHash = parent;
+        this.blobs = blobs;
+        List<String> list = new ArrayList<>(blobs.values());
+        String blobFileName = "";
+        for (String blob : list) {
+            blobFileName += blob;
+        }
+        this.timestamp = generatedDate(initial);
+        this.sha1 = Utils.sha1("COMMIT" + message + blobFileName);
+    }
+
+    private String generatedDate(boolean init) {
+        TimeZone tz = TimeZone.getTimeZone("PST");
+        Calendar cal = Calendar.getInstance(tz);
+        if (init == true) {
+            cal.setTimeInMillis(0);
+        }
+        DateFormat sdf = new SimpleDateFormat("EEE LLL d HH:mm:ss y Z");
+        sdf.setTimeZone(tz);
+        return sdf.format(cal.getTime());
+    }
+
+    /**
+     * Init commit construction
+     * 
+     * @param message
+     * @param map
+     */
+    public Commit(String message, HashMap<String, String> blobs) {
+        initFlag = true;
+        this.message = message;
+        this.blobs = blobs;
+        this.sha1 = Utils.sha1("COMMIT" + this.message);
+    }
+
+    public String getSHA1() {
+        return this.sha1;
+    }
+
+    public void save() {
+        File commitFile = Utils.join(Repositories.COMMITS_FOLDER, this.sha1);
+        Utils.writeObject(commitFile, this);
     }
 }
