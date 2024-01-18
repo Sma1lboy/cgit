50b016be4951cd288333c8da5314bd2d5100878b
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-17 20:28:20 -0800

    feat: branch, get latest commit of branch. get head sha1, get branch name, check branch exist

--- src/gitlet/Branch.java
+++ src/gitlet/Branch.java
@@ -1,8 +1,37 @@
 package gitlet;
 
+import java.io.Serializable;
+
 /**
  * Branch class help monitor which latest head we are at
  */
-public class Branch {
+public class Branch implements Serializable {
+    private String branchName;
+    private Commit head; // current branch latest update commit
 
+    public Branch(String branchName, Commit commit) {
+        this.branchName = branchName;
+        this.head = commit;
+    }
+
+    public Commit getHead() {
+        return this.head;
+    }
+
+    public String getHeadSHA1() {
+        return this.head.getSHA1();
+    }
+
+    public String getBranchName() {
+        return this.branchName;
+    }
+
+    // gethead
+    // getheadSHA1
+    // getbranchname
+    // create
+    // load
+    // save
+
+    // static hasbranch
 }
