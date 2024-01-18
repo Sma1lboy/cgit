bc39e9d8e6e8801df514ad29c47f316013865992
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-17 20:27:48 -0800

    feat: head to set global head and branch head

--- src/gitlet/Head.java
+++ src/gitlet/Head.java
@@ -1,10 +1,40 @@
 package gitlet;
 
+import java.io.File;
+
 /**
  * Head class keep manager which branch we are at
  */
 public class Head {
 
+    public static void setGlobalHEAD(String branchName, Commit commit) {
+        Branch branch = new Branch(branchName, commit);
+        Utils.writeObject(Repositories.HEAD, branch);
+    }
 
-    public static 
+    /**
+     * get the latest head commit in current HEAD pointer branch
+     * 
+     * @return
+     */
+    public static Commit getGlobalHEAD() {
+        Branch branch = Utils.readObject(Repositories.HEAD, Branch.class);
+        return branch.getHead();
+    }
+
+    public static void setBranchHEAD(String branchName, Commit commit) {
+        Branch branch = new Branch(branchName, commit);
+        File branchFile = Utils.join(Repositories.HEAD_REFS_FOLDER, branchName);
+        Utils.writeObject(branchFile, branch);
+    }
+
+    public static Commit getBranchHead(String branchName) {
+        File branchFile = Utils.join(Repositories.HEAD_REFS_FOLDER, branchName);
+        if (!branchFile.exists()) {
+            // todo doesnt exist
+            System.out.println("");
+        }
+        Branch branch = Utils.readObject(branchFile, Branch.class);
+        return branch.getHead();
+    }
 }
