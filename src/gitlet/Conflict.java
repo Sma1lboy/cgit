0fcd6c00df68932030e9a394029da23e95480db0
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-26 19:16:22 -0600

    to: adding new class Conflict to help resolve conflict between 2 blobs

---
+++ src/gitlet/Conflict.java
@@ -0,0 +1,20 @@
+package gitlet;
+
+import java.io.File;
+
+/**
+ * Conflict
+ */
+public class Conflict {
+    /**
+     * Reslove the conflict from same filepath but 2 different commit version
+     * 
+     * @param filepath
+     * @param blob1
+     * @param blob2
+     */
+    public static void reslove(File filepath, Blob headBlob, String headBranchName, Blob givenBlob,
+            String givenBranchName) {
+
+    }
+}
