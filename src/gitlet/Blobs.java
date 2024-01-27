d259c52fb1a3bda0782a50b2a8b6e8d943c0bc6f
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-26 19:15:59 -0600

    to: adding Blob helper method that get blob by blobSHA1 string

--- src/gitlet/Blobs.java
+++ src/gitlet/Blobs.java
@@ -7,8 +7,10 @@ public class Blobs {
     public static boolean containsBlobs(File file) {
         Blob testBlob = new Blob(file.getName(), file);
         File blobFile = Utils.join(Repositories.BLOB_FOLDER, testBlob.getSha1());
-
         return blobFile.exists();
+    }
 
+    public static Blob getBlob(String blobSHA1) {
+        return Utils.readObject(Utils.join(Repositories.BLOB_FOLDER, blobSHA1), Blob.class);
     }
 }
