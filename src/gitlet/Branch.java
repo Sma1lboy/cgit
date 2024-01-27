ccae96085cc4d1e9b98903fffa285c10fb349225
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-26 19:17:37 -0600

    adding instance method for find spitepoint with another branch base on current branch, also helper instance method list current branch's all commits

--- src/gitlet/Branch.java
+++ src/gitlet/Branch.java
@@ -1,6 +1,8 @@
 package gitlet;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Branch class help monitor which latest head we are at
@@ -26,6 +28,29 @@ public class Branch implements Serializable {
         return this.branchName;
     }
 
+    /**
+     * Return all the commit from current branch.
+     * 
+     * @return
+     */
+    public Map<String, Commit> listCommits() {
+        Map<String, Commit> map = new HashMap<>();
+        Commit curr = this.head;
+        while (curr.getDate() != null) {
+            map.put(curr.getSHA1(), curr);
+            curr = curr.getParent();
+        }
+        return map;
+    }
+
+    public Commit findSplitPoint(Branch branch) {
+        Map<String, Commit> commits = listCommits();
+        Commit head = branch.getHead();
+        while (!commits.containsKey(head.getSHA1())) {
+            head = head.getParent();
+        }
+        return head;
+    }
     // gethead
     // getheadSHA1
     // getbranchname
@@ -34,4 +59,5 @@ public class Branch implements Serializable {
     // save
 
     // static hasbranch
+
 }
